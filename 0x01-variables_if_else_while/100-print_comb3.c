86ee8621b3ba1a3f9c659da8edb5ee04bd404cd5
Yayeks <anhandle1@gmail.com>
2022-03-14 08:28:49 -0700

    Task10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <string.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+	for (j = i; j <= 9; j++)
+	{
+	if (i == j)
+	{
+	continue;
+	}
+	putchar(i%10 + '0');
+	putchar(j%10 + '0');
+	if (i == 8)
+	{
+	continue;
+	}
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	return (0);
+}
+
