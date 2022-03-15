de417672832fcdb40f5375721f07c68f47965a9e
Yayeks <anhandle1@gmail.com>
2022-03-15 19:55:45 +0000

    Task5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_sign - sign of a number
+ * @n: input
+ * Return: 1 and print + if n > 0
+ * 0 and print 0 if n == 0
+ * -1 and print - if n < 0
+ */
+int print_sign(int n);
+{
+	int answer;
+	
+	if (n > 0)
+	{
+	_putchar('+');
+	answer = 1;
+	}
+	if (n == 0)
+	{
+	_putchar('0')
+	answer = 0;
+	}
+	if (n < 0)
+	{
+	_putchar('-')
+	answer = -1
+	}
+	return (answer);
+}
