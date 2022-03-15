d6d7067f50bae3fdc02f5cd4092534544a6cc168
Yayeks <anhandle1@gmail.com>
2022-03-15 18:45:12 +0000

    Task 1 and Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,20 @@
 #include "main.h"
 /**
- * print_alphabet - abcd
- * print lowercase alphabet
- * Return: void.
+ * print_alphabet_x10 - abcd
+ * prints all aphabets in 10 lines
+ * Return: void
  */
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
-	int i;
+	int i, j;
 
+	for (j = 0; j < 10; j++)
+	{
 	for (i = 0; i < 26; i++)
 	{
 	_putchar(alphabet[i]);
 	}
 	_putchar('\n');
+	}
 }
