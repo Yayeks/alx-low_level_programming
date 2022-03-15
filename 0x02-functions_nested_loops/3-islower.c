b0f47e17b452bb1f0b78209fce4682b166159e1b
Yayeks <anhandle1@gmail.com>
2022-03-15 18:51:49 +0000

    Task3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower - check for lowercase letters
+ * Return: 1 if c is lowercase
+ * else return 0
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c >= 'z')
+{
+	return (1);
+}
+else
+{
+	return (0);
+}
+}
