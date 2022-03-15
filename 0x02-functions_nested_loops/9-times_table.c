7e7207a5296a0aa13ef2c16185487b32be11eb09
Yayeks <anhandle1@gmail.com>
2022-03-15 21:00:28 +0000

    Task9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * times_table - multiplication of 9
+ * Return: void.
+ */
+void times_table(void)
+{
+	int i, j, m;
+
+	for (i = 0; i < 10; i++)
+	{
+	for (j = 0; j < 10; j++)
+	{
+	m = i * j;
+	_putchar('0' + (m / 10));
+	_putchar('0' + (m % 10));
+	if (j == 9)
+	{
+		continue;
+	}
+	_putchar(',');
+	_putchar(' ');
+	}
+	_putchar('\n');
+	}
+}
