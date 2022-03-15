0c6c8bc041ed84881e03a187a1c59b0a7859d365
Yayeks <anhandle1@gmail.com>
2022-03-15 18:28:36 +0000

    Task1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,12 +6,12 @@
  */
 void print_alphabet(void)
 {
-	/*char alphabet[26] = "abcdefghijklmnopqrstuvwxyz"*/
-	char i;
+	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz"
+	int i;
 
-	for (i = "a"; i <= "z"; i++)
+	for (i = 0; i < 26; i++)
 	{
-	_putchar(i);
+	_putchar(alphabet[i]);
 	}
 	_putchar('\n');
 }
