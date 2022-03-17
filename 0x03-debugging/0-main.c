3a8c585ad6a70c1a05246b499611b8c291e36c61
Yayeks <anhandle1@gmail.com>
2022-03-17 11:38:34 -0700

    Task0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+void positive_or_negative(int i);
 /**
  * main - tests function that prints if integer is positive or negative
  * Return: 0
@@ -10,5 +10,6 @@ int main(void)
 
 	i =0;
 	positive_or_negative(i);
+
 	return (0);
 }
