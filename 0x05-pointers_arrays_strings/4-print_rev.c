bd464722d349a24795ac172f64f2fdf618a19b2b
Yayeks <anhandle1@gmail.com>
2022-03-21 14:08:59 -0700

    Task4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * print_rev - prints strin in reverse.
+ * @s: pointer to string.
+ * Return: void.
+ */
+void print_rev(char *s)
+{
+	strrev(s);
+}
