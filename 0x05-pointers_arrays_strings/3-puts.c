22d8b39015f67f7d9d25b75d6e3067f4066bf0bc
Yayeks <anhandle1@gmail.com>
2022-03-21 10:39:57 -0700

    Task3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _puts - prints string followed by newline.
+ * @str: pointer to string.
+ * Return: void.
+ */
+void _puts(char *str)
+{
+	puts( *str);
+}
