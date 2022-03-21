44c23c13292b3cbc4c3e1acd7843ae83fe9a0c5c
Yayeks <anhandle1@gmail.com>
2022-03-21 15:59:02 -0700

    task9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strcpy - copy stringat the end of another string including \0.
+ * @dest: pointer to string to be copied to.
+ * @src: pointer to string to be copied.
+ * Return: pointer to dest.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	memset(dest, '\0', sizeof(dest));
+	strcpy(dest, src);
+	return(dest);
+}
