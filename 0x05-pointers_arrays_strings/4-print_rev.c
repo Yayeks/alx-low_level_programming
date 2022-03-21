28a93a1c46770ba7b34ab1047ae35ee214e6b1f9
Yayeks <anhandle1@gmail.com>
2022-03-21 14:42:28 -0700

    Task4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,5 +8,17 @@
  */
 void print_rev(char *s)
 {
-	strrev(s);
+	int length, last;
+
+	length = 0;
+	while (s[length] != '\0')
+	{
+	length++;
+	}
+	last = length - 1;
+	for (; last >= 0; last--)
+	{
+	_putchar(s[last]);
+	}
+	_putchar('\n');
 }
