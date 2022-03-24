e84def9358188c46436b08948b2d39c56aeed095
Yayeks <anhandle1@gmail.com>
2022-03-24 02:23:32 -0700

    Task6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes every first letter of a word in a string.
+ * separators of words are:  space, tabulation,
+ * new line, ,, ;, ., !, ?, '', (, ), {, and }.
+ * @m: pointer to string.
+ * Return: pointer to m.
+ */
+char *cap_string(char *m)
+{
+	int i;
+
+	/* capitalize first character of words */
+	for (i = 0; m[i] != '\0'; i++)
+	{
+	if (i == 0)
+	{
+	if ((m[i] >= 'a' && m[i] <= 'z'))
+	{
+	m[i] = m[i] - 32;
+	continue;
+	}
+	if (m[i] == ' ' || m[i] == '\t' || m[i] == '\n' || m[i] == ','
+			|| m[i] == ';' || m[i] == '.' || m[i] == '!'
+			|| m[i] == '?' || m[i] == '"' || m[i] == '('
+			|| m[i] == ')' || m[i] == '{' || m[i] == '}')
+	{
+	++i;
+	if (m[i] >= 'a' && m[i] <= 'z')
+	{
+	m[i] = m[i] - 32;
+	continue;
+	}
+	}
+	}
+	}
+	return (m);
+}
