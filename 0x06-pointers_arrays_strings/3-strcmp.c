62ca24a6b5a29a61049fa0149e1c3a8465cd089b
Yayeks <anhandle1@gmail.com>
2022-03-24 00:30:47 -0700

    Task3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,17 +14,6 @@ int _strcmp(char *s1, char *s2)
 	int ret;
 	
 	ret = strcmp(s1, s2);
-	if (ret < 0)
-	{
-	return (-15);
-	}
-	else if (ret > 0)
-	{
-	return (15);
-	}
-	else
-	{
-	return (0);
-	}
+	return (ret);
 }
 
