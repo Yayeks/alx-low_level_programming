02687382aeabf7ba4a797220780a038daa882468
Yayeks <anhandle1@gmail.com>
2022-03-24 03:55:52 -0700

    Task6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -20,6 +20,7 @@ char *cap_string(char *m)
 	m[i] = m[i] - 32;
 	continue;
 	}
+	}
 	if (m[i] == ' ' || m[i] == '\t' || m[i] == '\n' || m[i] == ','
 			|| m[i] == ';' || m[i] == '.' || m[i] == '!'
 			|| m[i] == '?' || m[i] == '"' || m[i] == '('
@@ -30,7 +31,6 @@ char *cap_string(char *m)
 	{
 	m[i] = m[i] - 32;
 	continue;
-	}
 	}
 	}
 	}
