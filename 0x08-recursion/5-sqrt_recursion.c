0ff925e4ebd246097a4b3182577751e116fb9746
Yayeks <anhandle1@gmail.com>
2022-03-30 06:46:26 -0700

    Square root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,6 +9,11 @@ int _sqrt_recursion(int n)
 {
 	return (square(n, 0));
 }
+/**
+ * square - checks if square of x is n
+ * @x: value to check
+ * Return: int
+ */
 int square(int n, int x)
 {
 	if (x * x == n)
