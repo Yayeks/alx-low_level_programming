4262d148549e49c1ec686632d4dae903d9fdd334
Yayeks <anhandle1@gmail.com>
2022-03-30 07:56:57 -0700

    Prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -4,21 +4,33 @@
  * @n: number to check
  * Return: int
  */
-int div(int n, int x);
-
+/**
+ * div - check for prime
+ * @n: number
+ * @x: value
+ * Return: int
+ */
 int div(int n, int x)
 {
 	if (n <= 1)
+	{
 	return (0);
+	}
 	if (x != n)
 	{
 	if (n % x != 0)
+	{
 	return (div(n, x+1));
-	else
-	return (0);
 	}
 	else
+	{
+	return (0);
+	}
+	}
+	else
+	{
 	return (1);
+	}
 }
 int is_prime_number(int n)
 {
