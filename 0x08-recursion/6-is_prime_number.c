22234a866acd876157a8a2d1f0a865966c1be9e7
Yayeks <anhandle1@gmail.com>
2022-03-30 07:44:51 -0700

    Prime Number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * is_prime_number - returns prime number
+ * @n: number to check
+ * Return: int
+ */
+int div(int n, int x);
+
+int div(int n, int x)
+{
+	if (n <= 1)
+	return (0);
+	if (x != n)
+	{
+	if (n % x != 0)
+	return (div(n, x+1));
+	else
+	return (0)
+	}
+	else
+	return (1);
+}
+int is_prime_number(int n)
+{
+	return (div(n, 2));
+}
