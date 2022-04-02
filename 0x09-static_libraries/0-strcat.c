717894dd1166f1e643f1b8a7510e44abe527f39e
Yayeks <anhandle1@gmail.com>
2022-04-01 19:55:40 -0700

    Task0

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strcat - function that concatenates two strings.
+ * @dest: pointer to destination string.
+ * @src: pointer to source string.
+ * Return: pointer to destination string.
+ */
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);
+	return (dest);
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _abs - absolute value of a number.
+ * @n: input.
+ * Return: absolute value.
+ */
+int _abs(int n)
+{
+	n = abs(n);
+	return (n);
+}
