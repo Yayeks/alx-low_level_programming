c39347462ec2ca7541f43e3d825d95013ee2fe57
Yayeks <anhandle1@gmail.com>
2022-04-06 01:22:37 -0700

    Fix

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -14,15 +14,13 @@ int count_word(char *s)
 
 	for (c = 0; s[c] != '\0'; c++)
 	{
-	if (s[c] == ' ')
-	{
-	flag = 0;
-	}
-	else if (flag == 0)
-	{
-	flag = 1;
-	w++;
-	}
+		if (s[c] == ' ')
+			flag = 0;
+		else if (flag == 0)
+		{
+			flag = 1;
+			w++;
+		}
 	}
 	return (w);
 }
@@ -38,45 +36,34 @@ char **strtow(char *str)
 	int i, k = 0, len = 0, words, c = 0, start, end;
 
 	while (*(str + len))
-	{
-	len++;
-	}
+		len++;
 	words = count_word(str);
 	if (words == 0)
-	{
-	return (NULL);
-	}
+		return (NULL);
+
 	matrix = (char **) malloc(sizeof(char *) * (words + 1));
 	if (matrix == NULL)
-	{
-	return (NULL);
-	}
+		return (NULL);
 	for (i = 0; i <= len; i++)
 	{
-	if (str[i] == ' ' || str[i] == '\0')
-	{
-	if (c)
-	{
-	end = i;
-	tmp = (char *) malloc(sizeof(char) * (c + 1));
-	if (tmp == NULL)
-	{
-	return (NULL);
-	}
-	while (start < end)
-	{
-	*tmp++ = str[start++];
-	}
-	*tmp = '\0';
-	matrix[k] = tmp - c;
-	k++;
-	c = 0;
-	}
-	}
-	else if (c++ == 0)
-	{
-	start = i;
-	}
+		if (str[i] == ' ' || str[i] == '\0')
+		{
+			if (c)
+			{
+				end = i;
+				tmp = (char *) malloc(sizeof(char) * (c + 1));
+				if (tmp == NULL)
+					return (NULL);
+				while (start < end)
+					*tmp++ = str[start++];
+				*tmp = '\0';
+				matrix[k] = tmp - c;
+				k++;
+				c = 0;
+			}
+		}
+		else if (c++ == 0)
+			start = i;
 	}
 	matrix[k] = NULL;
 	return (matrix);
