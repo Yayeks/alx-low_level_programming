d3dd5014da8f218e6428084beb56bed19ea3c1a7
Yayeks <anhandle1@gmail.com>
2022-04-11 12:35:44 -0700

    First Task

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * dog_a - Typedef for struct dog
+ */
+typedef struct dog dog_a;
+/**
+ * struct dog -structures that stores information about dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
