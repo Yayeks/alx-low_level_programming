b091b43b217a00324714fa0c9bf0244bd2c10f64
Yayeks <anhandle1@gmail.com>
2022-04-11 13:06:53 -0700

    Task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,9 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 /**
- * dog_a - Typedef for struct dog
+ * dog_t - Typedef for struct dog
  */
-typedef struct dog dog_a;
+/*typedef struct dog dog_t;*/
 /**
  * struct dog -structures that stores information about dog
  * @name: name of the dog
