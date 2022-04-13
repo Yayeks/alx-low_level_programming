63c79e63a91ba069d2fd0fed20b58d7721ec491b
Yayeks <anhandle1@gmail.com>
2022-04-13 13:46:49 -0700

    Task2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,10 +13,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (array == NULL || cmp == NULL)
 		return (-1);
+	
 	for (index = 0; index < size; index++)
 	{
 		if (cmp(array[index]) != 0)
 			return (index);
 	}
+
 	return (-1);
 }
