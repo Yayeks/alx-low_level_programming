d6d5bf42edc6e9e96cc4e64bd1ed158839635735
Yayeks <anhandle1@gmail.com>
2022-04-22 07:35:47 -0700

    task0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,7 +3,6 @@
 /**
  * print_list - Prints all the elements of a list_t list
  * @h: The list_t list
- * @node: the node
  * Return: The number of nodes in h
  */
 size_t print_list(const list_t *h)
