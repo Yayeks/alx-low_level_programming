f4ec437a36cab26d183a68b67dfe9ba268c8cba9
Yayeks <anhandle1@gmail.com>
2022-04-22 04:24:12 -0700

    Header file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,14 +1,15 @@
 #ifndef LISTS_H
 #define LISTS_H
 /**
- * Desc: header file containinf prototypes and definitions
- * for all functions and types written in this pro/
+ * Desc: header file containinf prototypes and definition
+ * for all functions and types written in this proj
  */
 #include <stdlib.h>
+
 /**
- * struct list_s - singly linked list@str: string - (malloc'ed string)
- * @len: length of the string
+ * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
+ * @len: length of the string
  * @next: points to the next node
  * Description: singly linked list node structure for Holberton project
  */
