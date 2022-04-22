cd10e477db339540aea34be5d50dcb7a5760677c
Yayeks <anhandle1@gmail.com>
2022-04-22 04:10:33 -0700

    Task2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,17 +9,25 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
 	size_t nchar;
+	int len;
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
-	new->str = strdup(str);
+	nchar = strdup(str);
 
-	for (nchar = 0; str[nchar]; nchar++)
-		;
-	new->len = nchar;
+	if (nchar == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	for (len = 0; str[len];)
+		len++;
+	new->str = nchar;
+	new->len = len;
 	new->next = *head;
+
 	*head = new;
 
-	return (*head);
+	return (new);
 }
