92ac4decaaea92d12b68a61beb759859e5890ee4
Yayeks <anhandle1@gmail.com>
2022-04-22 03:53:42 -0700

    Task1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * list_len - Finds the number of elements in a linked list_t list
+ * @h: The linked list_t list
+ * @element: a counter var for size_t (nodes)
+ * Return: The number of elements in h.
+ */
+size_t list_len(const list_t *h)
+{
+	size_t element = 0;
+
+	while (h)
+	{
+		element++;
+		h = h->next;
+	}
+	return (element);
+}
