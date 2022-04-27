9b0a54dadf2db46e5b708059d8850e08a37f15b5
Yayeks <anhandle1@gmail.com>
2022-04-27 12:22:49 -0700

    Edited

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,13 +7,13 @@ int main(void)
 {
 	long int i, x = 1, y = 2, sum = 0;
 
-	for (i = 1; i < 91; i++)
+	for (i = 0; i < 97; i++)
 	{
 	printf("%lu, ", x);
 	sum = x + y;
 	x = y;
 	y = sum;
-	if (i == 93)
+	if (i == 97)
 	{
 	printf("%lu\n", x);
 	}
