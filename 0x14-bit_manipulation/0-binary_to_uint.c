a9bda23629c1ac8127a181f1a9d5e05667936e3d
Yayeks <anhandle1@gmail.com>
2022-04-28 04:07:34 -0700

    Task0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,7 +10,7 @@ unsigned int binary_to_uint(const char *b)
 
 	if (b[len] == '\0')
 		return (0);
-	while ((b[len] == '0' || b[len] == '1'))
+	while ((b[len] == '0') || (b[len] == '1'))
 	{
 		no <<= 1;
 		no += b[len] - '0';
