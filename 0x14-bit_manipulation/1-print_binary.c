53ebb12b53c8f8ae70274630fd595a5134887469
Yayeks <anhandle1@gmail.com>
2022-04-28 05:58:55 -0700

    Task1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
- * print_binary - print the binary rep of number
- * @n: the number to be interpreted
+ * print_binary - the binary representation of a number
+ * @n: the number to be represented
  */
 void print_binary(unsigned long int n)
 {
 	if (n > 1)
 		print_binary(n >> 1);
+
 	_putchar((n & 1) + '0');
 }
