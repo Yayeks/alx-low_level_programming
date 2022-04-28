e14ffb7470948ecd739960088379cca9958b5044
Yayeks <anhandle1@gmail.com>
2022-04-28 06:45:42 -0700

    Task2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * get_bit - get the value of a bit at a given index
+ * @n: number to be converted
+ * @index: the index
+ * Return: the value of the bit at index index or -1 if an error occured
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+	if (((n >> index) & 1) == 0)
+		return (0);
+	else
+		return(1);
+}
