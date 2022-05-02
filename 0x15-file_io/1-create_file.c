ef14800b83688f724486f0bf2c766881f35f0a30
Yayeks <anhandle1@gmail.com>
2022-05-02 10:17:29 -0700

    Task1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * create_file - creates a file
- * @filename - pointer to the filename
- * @text_content - NULL terminated string to write to the file
- * Return: 1 on success, -1 on failure
+ * create_file - Creates a file
+ * @filename: A pointer to the name of the file to create
+ * @text_content: A pointer to a string to write to the file
+ * Return: If the function fails - -1
  */
 int create_file(const char *filename, char *text_content)
 {
