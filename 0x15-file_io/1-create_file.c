ad701cd7f1868654a8387173e709f613104a99f0
Yayeks <anhandle1@gmail.com>
2022-05-02 10:21:22 -0700

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,6 +12,7 @@ int create_file(const char *filename, char *text_content)
 	len = 0;
 	if (filename == NULL)
 		return (0);
+
 	if (text_content != NULL)
 	{
 		for (len, text_content[len];)
@@ -23,6 +24,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (o == -1 || w == -1)
 		return (-1);
+
 	close(o);
 	return (1);
 }
