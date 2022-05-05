3b91c2e80ba1fafd6f4f6c140c3903657e3ae647
Yayeks <anhandle1@gmail.com>
2022-05-05 02:36:11 -0700

    task0"

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,10 +1,11 @@
 #include "main.h"
 #include <stdlib.h>
+
 /**
- * read_textfile - Reads text file and prints
- * @filename: A pointer to the name of file
- * @letters: the letters
- * Return: 0 if file cannot be opened
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename
+ * @letters: numbers of letters printed
+ * Return: numbers of letters printed. It fails, returns 0
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
